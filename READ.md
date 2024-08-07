39e25e63573485bff14e9f6c445c876986463153
brunomaion <brunosmaion@hotmail.com>
2024-08-07 01:03:06 -0300

    commit1

---
+++ READ.md
@@ -0,0 +1,14 @@
+# DOTNET (.NET) ESTUDOS
+
+## Passo 1: Instalar o .NET SDK
+Se você ainda não instalou o .NET SDK, baixe e instale a versão mais recente do site oficial da Microsoft.
+
+## Passo 2: Criar um Projeto
+Navegue até a pasta onde você deseja criar o projeto e execute o seguinte comando no terminal:
+
+<code> dotnet new console -n MeuProjeto
+
+Isso cria uma nova pasta chamada MeuProjeto com um projeto de console dentro dela.
+
+## Passo 3: Para Executar
+<code> dotnet run
---
+++ estudo1/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// inteiro
+int var1 = 2;
+Console.WriteLine(var1);
+
+// double
+double var2 = 2.345;
+Console.WriteLine(var2);
+Console.WriteLine(var1 + var2);
+
+// cast
+Console.WriteLine(var1 + (int)var2);
+
+// string
+string string1 = "teste";
+string string2 = "dotnet";
+Console.WriteLine(string1 + " " + string2);
+
+Console.WriteLine(string1 + " " + var1);
+
+// if
+
+//double var3 = 2.0000000000000001; //18
+double var3 = 2.000000000000001;  //17
+Console.WriteLine(var3);
+if (var3>2)
+{
+  Console.WriteLine("Verdade!!");
+} 
+else 
+{
+  Console.WriteLine("Falso!!");
+}
+
+// for
+int tamanho = 10;
+string aux = "";
+for (int i = 0; i < tamanho; i++){
+    aux = aux + i.ToString();
+    Console.Write(aux);
+}
